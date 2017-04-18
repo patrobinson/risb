011fb0a0fef364eb9cf29aeadbfeb6229a1acc1a
Patrick Robinson <patrick.robinson@envato.com>
2017-04-18 22:13:53 +1000

    iterate through pages

--- strava.go
+++ strava.go
@@ -8,7 +8,33 @@ import (
 	"time"
 )
 
-func pull() error {
+func pull() {
+	log.Info("Pulling...\n")
+	accessToken := os.Getenv("STRAVA_ACCESS_TOKEN")
+	client := strava.NewClient(accessToken)
+	cAthlete := strava.NewCurrentAthleteService(client)
+	page := 1
+	for {
+		summaries, err := cAthlete.ListActivities().Page(page).PerPage(200).Do()
+		if err != nil {
+			log.Fatal("Failed to get activities %v", err)
+		}
+		if len(summaries) == 0 {
+			log.Info("End of activities reached at page %v", page)
+			break
+		}
+		page++
+		streams := strava.NewActivityStreamsService(client)
+		for _, s := range summaries {
+			err = getActivityStream(*s, streams)
+			if err != nil {
+				log.Info("Failed to get Activity %v: %v", s.Id, err)
+			}
+		}
+	}
+}
+
+func getActivityStream(activity strava.ActivitySummary, streams *strava.ActivityStreamsService) error {
 	streamTypes := []strava.StreamType{
 		"time",
 		"distances",
@@ -18,59 +44,47 @@ func pull() error {
 		"grade_smooth",
 		"velocity_smooth",
 	}
-	fmt.Printf("Pulling...\n")
-	accessToken := os.Getenv("STRAVA_ACCESS_TOKEN")
-	client := strava.NewClient(accessToken)
-	cAthlete := strava.NewCurrentAthleteService(client)
-	summaries, err := cAthlete.ListActivities().Do()
+	streamSets, err := streams.Get(activity.Id, streamTypes).Do()
 	if err != nil {
-		log.Fatal(fmt.Sprintf("Failed to get activities %v", err))
+		return err
 	}
-	streams := strava.NewActivityStreamsService(client)
-	for _, s := range summaries {
-		streamSets, err := streams.Get(s.Id, streamTypes).Do()
-		if err != nil {
-			fmt.Printf("Failed to get Activity %v: %v", s.Id, err)
-			continue
+	var previousTime int
+	previousTime = 0
+	for i, t := range streamSets.Time.RawData {
+		elapsed := (*t - previousTime)
+		data := dataPoint{
+			name: "run",
+			tags: map[string]string{
+				"Id": strconv.FormatInt(activity.Id, 10),
+				"Athlete": strconv.FormatInt(activity.Athlete.AthleteMeta.Id, 10),
+				"MovingTime": strconv.Itoa(activity.MovingTime),
+				"ElapsedTime": strconv.Itoa(activity.ElapsedTime),
+			},
+			timestamp: activity.StartDate.Add(time.Second * time.Duration(*t)),
+			precision: elapsed,
+			values: map[string]float64{
+				"Time": float64(*t),
+			},
 		}
-		var previousTime int
-		previousTime = 0
-		for i, t := range streamSets.Time.RawData {
-			elapsed := (*t - previousTime)
-			data := dataPoint{
-				name: "run",
-				tags: map[string]string{
-					"Id": strconv.FormatInt(s.Id, 10),
-					"Athlete": strconv.FormatInt(s.Athlete.AthleteMeta.Id, 10),
-					"MovingTime": strconv.Itoa(s.MovingTime),
-					"ElapsedTime": strconv.Itoa(s.ElapsedTime),
-				},
-				timestamp: s.StartDate.Add(time.Second * time.Duration(*t)),
-				precision: elapsed,
-				values: map[string]float64{
-					"Time": float64(*t),
-				},
-			}
-			if distance := streamSets.Distance.RawData[i]; distance != nil {
-				data.values["Distance"] = *distance
-				data.values["MinutesPerKM"] = (((1000 / *distance) * float64(elapsed)) / 60)
-			}
-			if speed := streamSets.Speed.RawData[i]; speed != nil {
-				data.values["MetersPerSecond"] = *speed
-			}
-			if altitude := streamSets.Elevation.RawData[i]; altitude != nil {
-				data.values["Altitude"] = *altitude
-			}
-			if heartrate := streamSets.HeartRate.RawData[i]; heartrate != nil {
-				data.values["HeartRate"] = float64(*heartrate)
-			}
-			if grade := streamSets.Grade.RawData[i]; grade != nil {
-				data.values["Grade"] = *grade
-			}
-			data.tags["Moving"] = strconv.FormatBool(streamSets.Moving.Data[i])
-			previousTime = *t
-			sink(data)
+		if distance := streamSets.Distance.RawData[i]; distance != nil {
+			data.values["Distance"] = *distance
+			data.values["MinutesPerKM"] = (((1000 / *distance) * float64(elapsed)) / 60)
 		}
+		if speed := streamSets.Speed.RawData[i]; speed != nil {
+			data.values["MetersPerSecond"] = *speed
+		}
+		if altitude := streamSets.Elevation.RawData[i]; altitude != nil {
+			data.values["Altitude"] = *altitude
+		}
+		if heartrate := streamSets.HeartRate.RawData[i]; heartrate != nil {
+			data.values["HeartRate"] = float64(*heartrate)
+		}
+		if grade := streamSets.Grade.RawData[i]; grade != nil {
+			data.values["Grade"] = *grade
+		}
+		data.tags["Moving"] = strconv.FormatBool(streamSets.Moving.Data[i])
+		previousTime = *t
+		sink(data)
 	}
 	return nil
 }
